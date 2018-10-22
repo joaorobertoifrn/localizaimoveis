91a740360449266bdd8f6d9e84dc2d492923a5fe
João Roberto de França Pereira <joao.roberto@academico.ifrn.edu.br>
2018-10-22 18:33:16 -0300

    Primeiro Commit - projeto criado

---
+++ src/main/java/br/edu/ifrn/localizaimovel/LocalizaimovelApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifrn.localizaimovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizaimovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizaimovelApplication.class, args);
+	}
+}
