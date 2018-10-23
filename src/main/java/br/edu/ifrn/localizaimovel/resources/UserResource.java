3356f3975e0ce6a191d25fb1c074be0e0a5e6a92
João Roberto de França Pereira <joao.roberto@academico.ifrn.edu.br>
2018-10-22 21:30:27 -0300

    Atualização de usuario com PUT

--- src/main/java/br/edu/ifrn/localizaimovel/resources/UserResource.java
+++ src/main/java/br/edu/ifrn/localizaimovel/resources/UserResource.java
@@ -51,4 +51,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User user = service.fromDTO(objDto);
+		user.setId(id);
+		user = service.update(user);
+		return ResponseEntity.noContent().build();
+	}
+
 }
